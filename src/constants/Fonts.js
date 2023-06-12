3896c7104d302aaa14e6fe77607f0b706716bec2
spdevpri <spdevpri@gmail.com>
2023-06-12 21:32:57 +0530

    complete font added

---
+++ src/constants/Fonts.js
@@ -0,0 +1,20 @@
+export const Fonts = {
+    /**400 */
+    regular: 'Inter-Regular',
+    /**500 */
+    medium: 'Inter-Medium',
+    /**300 */
+    light: 'Inter-Light',
+    /**200 */
+    extraLight: 'Inter-ExtraLight',
+    /**100 */
+    thin: 'Inter-Thin',
+    /**700 */
+    bold: 'Inter-Bold',
+    /**800 */
+    extraBold: 'Inter-ExtraBold',
+    /**600 */
+    semiBold: 'Inter-SemiBold',
+    /**900 */
+    black: 'Inter-Black'
+}

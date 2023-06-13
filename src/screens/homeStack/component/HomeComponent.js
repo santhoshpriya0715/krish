32b31900b9a43a560eaee6e5442955a3b31c8f1d
eventurer <eventurersandroidtest@gmail.com>
2023-06-13 21:10:14 +0530

    blog

--- src/screens/homeStack/component/HomeComponent.js
+++ src/screens/homeStack/component/HomeComponent.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import BaseSafeViewComp from '../../../baseComponent/BaseSafeViewComp'
 
 const HomeComponent = () => {
   return (
-    <View>
-      <Text>HomeComponent</Text>
-    </View>
+    <BaseSafeViewComp>
+    </BaseSafeViewComp>
   )
 }
 
